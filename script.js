29cb115e8de767a197308be26c7c5c0bae80950c
Racheal Ako <rachealako22@gmail.com>
2023-09-07 16:01:31 +0100

    i have completed the first page layout

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 var counter=1;
 let timer = setInterval(function(){
-    document.getElementById?("radio" + counter).checked=true;
+    document.getElementById("radio" + counter).checked=true;
         counter++;
         if(counter > 4){
             counter = 1;
         }
     },5000);
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // JavaScript for toggling the menu
+        const menuToggle = document.querySelector(".menu-toggle");
+        const menu = document.querySelector(".menu");
+
+        menuToggle.addEventListener("click", () => {
+            console.log("Button clicked");
+            menu.classList.toggle("active");
+        });
+    });
