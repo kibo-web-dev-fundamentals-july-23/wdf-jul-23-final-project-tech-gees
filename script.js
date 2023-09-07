07ddd7fb1f0a6122d4ebca990f80e6e643e20b72
Racheal Ako <rachealako22@gmail.com>
2023-09-07 18:14:17 +0100

    i am half way done with the second page

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ let timer = setInterval(function(){
             menu.classList.toggle("active");
         });
     });
+
+    // Get a reference to the refresh button
+const refreshButton = document.getElementById("refresh-button");
+
+// Add a click event listener to the button
+refreshButton.addEventListener("click", function() {
+    // Reload the current page
+    location.reload();
+});
+
+const toggleButton = document.getElementById("toggle-view");
+const viewIcon = document.getElementById("view-icon");
+const gridView = document.querySelector(".grid-view");
+const listView = document.querySelector(".list-view");
+
+let isGridView = true;
+
+toggleButton.addEventListener("click", function () {
+    if (isGridView) {
+        // Switch to list view
+        gridView.style.display = "none";
+        listView.style.display = "block";
+        viewIcon.classList.remove("fa-th-large");
+        viewIcon.classList.add("fa-list");
+    } else {
+        // Switch to grid view
+        gridView.style.display = "grid";
+        listView.style.display = "none";
+        viewIcon.classList.remove("fa-list");
+        viewIcon.classList.add("fa-th-large");
+    }
+
+    // Toggle the view state
+    isGridView = !isGridView;
+});
