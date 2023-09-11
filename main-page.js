81379bfabd9683ab4f7953e5ef21ed2fdb2955d8
Alexander Chosen Okon <chosenokonalexander@gmail.com>
2023-09-11 23:30:08 +0100

    updates made

--- main-page.js
+++ main-page.js
@@ -6,6 +6,7 @@ const menu = document.getElementById("menu");
 // Get the search results container
 const searchResults = document.querySelector(".search-results");
 const textContainer = document.querySelector(".text-container")
+const noOutput = document.getElementById("no-output")
 
 // Function to toggle the menu when the hamburger is clicked
 menuToggle.addEventListener("click", function() {
@@ -42,15 +43,6 @@ textarea.addEventListener('input', function () {
     this.style.height = (this.scrollHeight) + 'px';  // Set the height to match the content
 });
 
-
-// Function to open the note in an editable text box
-function openNoteInEditableTextBox(title, content) {
-  titleInput.value = title;
-  contentTextarea.value = content;
-  textArea.style.display = "block";
-  inputBox.style.display = "none";
-}
-
 // // This code will automatically adjust the height of the textarea as you type, increasing it to fit the content while keeping the width fixed at 300 pixels.
 
 // Get references to the input-box and text-area
@@ -60,6 +52,7 @@ const textArea = document.querySelector(".text-area");
 // Add a click event listener to the input-box
 inputBox.addEventListener("click", function () {
     // Toggle the visibility of input-box and text-area
+    noOutput.style.display = "none";
     inputBox.style.display = "none";
     textArea.style.display = "block";
 
@@ -75,12 +68,12 @@ document.getElementById("text-area").addEventListener("blur", function () {
 });
 
 // Add a click event listener to the document
-document.addEventListener("click", function (event) {
+  document.addEventListener("click", function (event) {
     // Check if the click event target is not inside the text container
     if (!textContainer.contains(event.target)) {
-        // Clicked outside the text container, hide the text area and show the input box
-        textArea.style.display = "none";
-        inputBox.style.display = "block";
+      // Clicked outside the text container, hide the text area and show the input box
+      textArea.style.display = "none";
+      inputBox.style.display = "block";
     }
 });
 
@@ -88,6 +81,7 @@ document.addEventListener("click", function (event) {
 // THIS IS FOR THE ICONS INSIDE THE TEXT AREA...
 
 // buttons in the text box
+const editableText = document.querySelector(".editable-text");
 const starButton = document.getElementById("star-button");
 let isStarred = false;
 
@@ -109,7 +103,7 @@ const noteContainer = document.getElementById("note-container");
 const titleInput = document.getElementById("title");
 const contentTextarea = document.getElementById("myTextarea");
 const saveButton = document.getElementById("saveButton");
-const noOutput = document.getElementById("no-output")
+
 
 // Add event listener to the save button
 saveButton.addEventListener("click", function() {
@@ -128,19 +122,30 @@ saveButton.addEventListener("click", function() {
       textContainer.style.marginTop = "10px"
       textArea.style.display = "none"
       inputBox.style.display ="block"
-
       noOutput.textContent = "Please Enter A Title and Text To Save"
+      const header = document.querySelector('header');
+      header.style.marginBottom = '10px';
+      setTimeout(function () {
+        noOutput.style.display = "none";
+    }, 15000);
     }
+
 });
+
+
 let notes =[]
 function addNote(title, content) {
     const note = {
       title: title,
       content: content
     };
-    noteContainer.style.display = "block"
+
+
+    noteContainer.style.display = "block";
     const noteDiv = document.createElement("div");
+    // noteContainer.appendChild(noteDiv);
     noteDiv.classList.add("notes");
+
 
     const titleDiv = document.createElement("div");
     titleDiv.classList.add("note-title");
@@ -154,19 +159,86 @@ function addNote(title, content) {
     dateDiv.classList.add("note-date");
     dateDiv.textContent = new Date().toLocaleString(); // Display the current date and time.
 
-    noteDiv.appendChild(titleDiv);
-    noteDiv.appendChild(contentDiv);
-    noteDiv.appendChild(dateDiv);
+    // new part
 
-    // Insert the new note at the top of the container.
-    noteContainer.insertBefore(noteDiv, noteContainer.firstChild);
+     // Add a click event listener to open the note using an editable text box
+      noteDiv.addEventListener("click", function () {
+        openNoteInEditableTextBox(title, content);
+      });
 
-    notes.push(note)
+      // Add a delete icon to the note div at the top right corner
+      const deleteIcon = document.createElement("i");
+      deleteIcon.classList.add("material-icons", "delete-icon");
+      deleteIcon.textContent = "delete";
+      console.log("delete icon created")
+      deleteIcon.addEventListener("click", function () {
+          deleteNoteAndMoveToTrash(noteContainer, note);
+      });
+
+
+      // Append the delete icon to the noteDiv
+      noteDiv.appendChild(deleteIcon);
+      noteDiv.appendChild(titleDiv);
+      noteDiv.appendChild(contentDiv);
+      noteDiv.appendChild(dateDiv);
+
+
+  // Insert the new note at the top of the container.
+  noteContainer.appendChild(noteDiv);
+  noteContainer.insertBefore(noteDiv, noteContainer.firstChild);
+
+  notes.push(note)
 }
 
-// FOR THE SEARCH FUNCTION...
+// Function to open the note in an editable text box
+function openNoteInEditableTextBox(title, content) {
+  titleInput.value = title;
+  contentTextarea.value = content;
+  textArea.style.display = "block";
+  inputBox.style.display = "none";
+}
 
-// Function to perform the search
+// function to delete note and move to trash
+function deleteNoteAndMoveToTrash(noteDiv, note) {
+  // Remove the note from the UI
+  noteDiv.remove();
+
+  // Remove the note from the notes array
+  const noteIndex = notes.indexOf(note);
+  if (noteIndex !== -1) {
+      notes.splice(noteIndex, 1);
+  }
+
+  // Create a new div for the deleted note in the trash menu
+  const trashNoteDiv = document.createElement("div");
+  trashNoteDiv.classList.add("trash-note");
+
+  // Create elements to display the deleted note's title and content
+  const titleDiv = document.createElement("div");
+  titleDiv.classList.add("note-title");
+  titleDiv.textContent = note.title;
+
+  const contentDiv = document.createElement("div");
+  contentDiv.classList.add("note-content");
+  contentDiv.textContent = note.content;
+
+  // Append the title and content to the trash note
+  trashNoteDiv.appendChild(titleDiv);
+  trashNoteDiv.appendChild(contentDiv);
+
+  // Append the trash note to the trash container
+  const trash = document.getElementById("trash");
+  trash.appendChild(trashNoteDiv);
+  trashNoteDiv.style.display = "none";
+}
+
+// function displayTrashNoteContainer()
+
+// // display the trashed notes
+// trash.addEventListener("click", function(){
+//   displayTrashNoteContainer()
+// })
+
 function searchNotes() {
   // Get the search input value
   const searchInput = document.getElementById("searchInput").value.toLowerCase();
@@ -181,7 +253,9 @@ function searchNotes() {
       // No matching notes found, display an error message
       noteContainer.style.display ="none"
       searchResults.innerHTML = "<h3>No matching notes found.</h3>";
-
+      setTimeout(function () {
+        searchResults.style.display = "none";
+    }, 15000);
   } else {
       // Display the matching notes
       noteContainer.style.display = "none"
@@ -189,7 +263,6 @@ function searchNotes() {
       filteredNotes.forEach(note => {
           const noteDiv = document.createElement("div");
           noteDiv.classList.add("note");
-          // const firstSentence = getFirstSentence(note.content);
           noteDiv.innerHTML = `<h3>${note.title}</h3><p>${note.content}</p>`;
           searchResults.appendChild(noteDiv);
   })
@@ -202,7 +275,7 @@ searchButton.addEventListener("click", searchNotes);
 
 // Add an event listener for the Enter key press event
 const searchInput = document.getElementById("searchInput");
-searchInput.addEventListener("keydown", function (event) {
+searchInput.addEventListener("keydown", function(event) {
   if (event.key === "Enter") {
       searchNotes();
   }
