48eff96510e9a44435c70f4a34d16d8228573950
Alexander Chosen Okon <chosenokonalexander@gmail.com>
2023-09-11 04:54:13 +0100

    completed some functionalities

--- main-page.js
+++ main-page.js
@@ -3,6 +3,9 @@
 // Get references to the menu elements
 const menuToggle = document.getElementById("menuToggle");
 const menu = document.getElementById("menu");
+// Get the search results container
+const searchResults = document.querySelector(".search-results");
+const textContainer = document.querySelector(".text-container")
 
 // Function to toggle the menu when the hamburger is clicked
 menuToggle.addEventListener("click", function() {
@@ -30,15 +33,16 @@ inputBox.addEventListener("click", function () {
     textArea.style.display = "block";
 
     // Focus on the text area for user input
-    document.getElementById("myTextarea").focus();
+    document.getElementById("text-area").focus();
 });
 
 // Add a blur event listener to the text area
-document.getElementById("myTextarea").addEventListener("blur", function () {
+document.getElementById("text-area").addEventListener("blur", function () {
     // Toggle the visibility of text-area and input-box
     inputBox.style.display = "block";
     textArea.style.display = "none";
 });
+
 
 
 // This code will automatically adjust the height of the textarea as you type, increasing it to fit the content while keeping the width fixed at 300 pixels.
@@ -78,31 +82,146 @@ starButton.addEventListener("click", () => {
 
 const noteContainer = document.getElementById("note-container");
 
+// Select the elements
+const titleInput = document.getElementById("title");
+const contentTextarea = document.getElementById("myTextarea");
+const saveButton = document.getElementById("saveButton");
+const noOutput = document.getElementById("no-output")
+
+// Add event listener to the save button
+saveButton.addEventListener("click", function() {
+    const title = titleInput.value;
+    const content = contentTextarea.value;
+
+    if (title && content) {
+        addNote(title, content);
+
+        // Clear the input fields after adding the note
+        titleInput.value = "";
+        contentTextarea.value = "";
+    } else {
+      noOutput.style.display = "block"
+      textContainer.style.marginTop = "10px"
+
+      noOutput.textContent = "Please Enter A Title and Text To Save"
+    }
+});
+let notes =[]
 function addNote(title, content) {
-  const noteDiv = document.createElement("div");
-  noteDiv.classList.add("note");
+    const note = {
+      title: title,
+      content: content
+    };
+    noteContainer.style.display = "block"
+    const noteDiv = document.createElement("div");
+    noteDiv.classList.add("notes");
 
-  const titleDiv = document.createElement("div");
-  titleDiv.classList.add("note-title");
-  titleDiv.textContent = title;
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("note-title");
+    titleDiv.textContent = title;
 
-  const contentDiv = document.createElement("div");
-  contentDiv.textContent = content.substring(0, content.indexOf(".") + 1); // Display the first sentence.
+    const contentDiv = document.createElement("div");
+    // contentDiv.textContent = content.substring(0, content.indexOf(".") + 1); // Display the first sentence.
+    contentDiv.textContent = content;
 
-  const dateDiv = document.createElement("div");
-  dateDiv.classList.add("note-date");
-  dateDiv.textContent = new Date().toLocaleString(); // Display the current date and time.
+    const dateDiv = document.createElement("div");
+    dateDiv.classList.add("note-date");
+    dateDiv.textContent = new Date().toLocaleString(); // Display the current date and time.
 
-  noteDiv.appendChild(titleDiv);
-  noteDiv.appendChild(contentDiv);
-  noteDiv.appendChild(dateDiv);
+    noteDiv.appendChild(titleDiv);
+    noteDiv.appendChild(contentDiv);
+    noteDiv.appendChild(dateDiv);
 
-  // Insert the new note at the top of the container.
-  noteContainer.insertBefore(noteDiv, noteContainer.firstChild);
+    // Insert the new note at the top of the container.
+    noteContainer.insertBefore(noteDiv, noteContainer.firstChild);
+
+    notes.push(note)
 }
 
-// Example usage:
-addNote("Note 1 Title", "This is the content of Note 1. It's a sample note.");
-addNote("Note 2 Title", "This is the content of Note 2. Another example note.");
-addNote("Note 3 Title", "This is the content of Note 3. It's a sample note.");
-addNote("Note 4 Title", "This is the content of Note 4. Another example note.");
+
+
+
+// function getFirstSentence(text) {
+//   const sentences = text.split(/[.!?]/); // Split the text into sentences
+//   if (sentences.length > 0) {
+//       return sentences[0].trim(); // Return the first sentence
+//   }
+//   return text; // If no sentences are found, return the entire text
+// }
+// Function to perform the search
+function searchNotes() {
+  // Get the search input value
+  const searchInput = document.getElementById("searchInput").value.toLowerCase();
+
+  // Filter the notes based on the search input
+  const filteredNotes = notes.filter(note => note.title.toLowerCase().includes(searchInput));
+
+  // Clear any previous results or error messages
+  searchResults.innerHTML = "";
+
+  if (filteredNotes.length === 0) {
+      // No matching notes found, display an error message
+      noteContainer.style.display ="none"
+      searchResults.innerHTML = "<h3>No matching notes found.</h3>";
+
+  } else {
+      // Display the matching notes
+      noteContainer.style.display = "none"
+      searchResults.style.display = "block"
+      filteredNotes.forEach(note => {
+          const noteDiv = document.createElement("div");
+          noteDiv.classList.add("note");
+          // const firstSentence = getFirstSentence(note.content);
+          noteDiv.innerHTML = `<h3>${note.title}</h3><p>${note.content}</p>`;
+          searchResults.appendChild(noteDiv);
+  })
+}
+}
+
+
+
+//new
+// function searchNotes() {
+//   // Get the search input value
+//   const searchInput = document.getElementById("searchInput").value.toLowerCase();
+
+//   // Select the note container and search results elements
+//   const noteContainer = document.querySelector(".note-container");
+//   const searchResults = document.querySelector(".search-results");
+
+//   // Filter the notes based on the search input
+//   const noteElements = noteContainer.querySelectorAll(".note");
+//   const filteredNotes = Array.from(noteElements).filter(noteElement => {
+//       const title = noteElement.querySelector(".note-title").textContent.toLowerCase();
+//       return title.includes(searchInput);
+//   });
+
+//   // Clear any previous results or error messages
+//   searchResults.innerHTML = "";
+
+//   if (filteredNotes.length === 0) {
+//       // No matching notes found, display an error message
+//       noteContainer.style.display = "none";
+//       searchResults.style.display = "block";
+//       searchResults.innerHTML = "<h3>No matching notes found.</h3>";
+//   } else {
+//       // Display the matching notes
+//       noteContainer.style.display = "none";
+//       searchResults.style.display = "block";
+//       filteredNotes.forEach(noteElement => {
+//           searchResults.appendChild(noteElement.cloneNode(true));
+//       });
+//   }
+// }
+
+// // Add an event listener for the search icon click event
+// const searchButton = document.querySelector(".search-button");
+// searchButton.addEventListener("click", searchNotes);
+
+// // Add an event listener for the Enter key press event
+// const searchInput = document.getElementById("searchInput");
+// searchInput.addEventListener("keydown", function (event) {
+//   if (event.key === "Enter") {
+//       searchNotes();
+//   }
+// });
